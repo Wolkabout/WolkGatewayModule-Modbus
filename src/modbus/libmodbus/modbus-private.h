3a826a5914404d1f986fbe2fecfa4456e1b074c5
LazarBozic94 <lazar.bozic@wolkabout.com>
2019-03-22 11:31:43 +0100

    Fixed the coil reading

--- src/modbus/libmodbus/modbus-private.h
+++ src/modbus/libmodbus/modbus-private.h
@@ -8,15 +8,15 @@
 #define MODBUS_PRIVATE_H
 
 #ifndef _MSC_VER
-#include <stdint.h>
-#include <sys/time.h>
+# include <stdint.h>
+# include <sys/time.h>
 #else
-#include "stdint.h"
-#include <time.h>
+# include "stdint.h"
+# include <time.h>
 typedef int ssize_t;
 #endif
-#include <config.h>
 #include <sys/types.h>
+#include <config.h>
 
 #include "modbus.h"
 
@@ -36,12 +36,11 @@ MODBUS_BEGIN_DECLS
 #define _MODBUS_EXCEPTION_RSP_LENGTH 5
 
 /* Timeouts in microsecond (0.5 s) */
-#define _RESPONSE_TIMEOUT 500000
-#define _BYTE_TIMEOUT 500000
+#define _RESPONSE_TIMEOUT    500000
+#define _BYTE_TIMEOUT        500000
 
-typedef enum
-{
-    _MODBUS_BACKEND_TYPE_RTU = 0,
+typedef enum {
+    _MODBUS_BACKEND_TYPE_RTU=0,
     _MODBUS_BACKEND_TYPE_TCP
 } modbus_backend_type_t;
 
@@ -50,8 +49,7 @@ typedef enum
  *  | Client | ---------------------->| Server |
  *  ---------- Confirmation  Response ----------
  */
-typedef enum
-{
+typedef enum {
     /* Request message on the server side */
     MSG_INDICATION,
     /* Request message on the client side */
@@ -60,38 +58,38 @@ typedef enum
 
 /* This structure reduces the number of params in functions and so
  * optimizes the speed of execution (~ 37%). */
-typedef struct _sft
-{
+typedef struct _sft {
     int slave;
     int function;
     int t_id;
 } sft_t;
 
-typedef struct _modbus_backend
-{
+typedef struct _modbus_backend {
     unsigned int backend_type;
     unsigned int header_length;
     unsigned int checksum_length;
     unsigned int max_adu_length;
-    int (*set_slave)(modbus_t* ctx, int slave);
-    int (*build_request_basis)(modbus_t* ctx, int function, int addr, int nb, uint8_t* req);
-    int (*build_response_basis)(sft_t* sft, uint8_t* rsp);
-    int (*prepare_response_tid)(const uint8_t* req, int* req_length);
-    int (*send_msg_pre)(uint8_t* req, int req_length);
-    ssize_t (*send)(modbus_t* ctx, const uint8_t* req, int req_length);
-    int (*receive)(modbus_t* ctx, uint8_t* req);
-    ssize_t (*recv)(modbus_t* ctx, uint8_t* rsp, int rsp_length);
-    int (*check_integrity)(modbus_t* ctx, uint8_t* msg, const int msg_length);
-    int (*pre_check_confirmation)(modbus_t* ctx, const uint8_t* req, const uint8_t* rsp, int rsp_length);
-    int (*connect)(modbus_t* ctx);
-    void (*close)(modbus_t* ctx);
-    int (*flush)(modbus_t* ctx);
-    int (*select)(modbus_t* ctx, fd_set* rset, struct timeval* tv, int msg_length);
-    void (*free)(modbus_t* ctx);
+    int (*set_slave) (modbus_t *ctx, int slave);
+    int (*build_request_basis) (modbus_t *ctx, int function, int addr,
+                                int nb, uint8_t *req);
+    int (*build_response_basis) (sft_t *sft, uint8_t *rsp);
+    int (*prepare_response_tid) (const uint8_t *req, int *req_length);
+    int (*send_msg_pre) (uint8_t *req, int req_length);
+    ssize_t (*send) (modbus_t *ctx, const uint8_t *req, int req_length);
+    int (*receive) (modbus_t *ctx, uint8_t *req);
+    ssize_t (*recv) (modbus_t *ctx, uint8_t *rsp, int rsp_length);
+    int (*check_integrity) (modbus_t *ctx, uint8_t *msg,
+                            const int msg_length);
+    int (*pre_check_confirmation) (modbus_t *ctx, const uint8_t *req,
+                                   const uint8_t *rsp, int rsp_length);
+    int (*connect) (modbus_t *ctx);
+    void (*close) (modbus_t *ctx);
+    int (*flush) (modbus_t *ctx);
+    int (*select) (modbus_t *ctx, fd_set *rset, struct timeval *tv, int msg_length);
+    void (*free) (modbus_t *ctx);
 } modbus_backend_t;
 
-struct _modbus
-{
+struct _modbus {
     /* Slave address */
     int slave;
     /* Socket or file descriptor */
@@ -100,18 +98,18 @@ struct _modbus
     int error_recovery;
     struct timeval response_timeout;
     struct timeval byte_timeout;
-    const modbus_backend_t* backend;
-    void* backend_data;
+    const modbus_backend_t *backend;
+    void *backend_data;
 };
 
-void _modbus_init_common(modbus_t* ctx);
-void _error_print(modbus_t* ctx, const char* context);
-int _modbus_receive_msg(modbus_t* ctx, uint8_t* msg, msg_type_t msg_type);
+void _modbus_init_common(modbus_t *ctx);
+void _error_print(modbus_t *ctx, const char *context);
+int _modbus_receive_msg(modbus_t *ctx, uint8_t *msg, msg_type_t msg_type);
 
 #ifndef HAVE_STRLCPY
-size_t strlcpy(char* dest, const char* src, size_t dest_size);
+size_t strlcpy(char *dest, const char *src, size_t dest_size);
 #endif
 
 MODBUS_END_DECLS
 
-#endif /* MODBUS_PRIVATE_H */
+#endif  /* MODBUS_PRIVATE_H */
