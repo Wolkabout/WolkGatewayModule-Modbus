bd56b7162a933ecfd1d2821e84b3be9f31a152eb
Danilo Ješić <34022788+djesic@users.noreply.github.com>
2019-04-03 10:27:55 +0200

    Registration rework update (#17)
    * Updated submodule
    
    * Formatting
    
    * Removed duplicate tests
    
    * Registration rework sync

--- src/modbus/libmodbus/modbus-tcp-private.h
+++ src/modbus/libmodbus/modbus-tcp-private.h
@@ -7,15 +7,16 @@
 #ifndef MODBUS_TCP_PRIVATE_H
 #define MODBUS_TCP_PRIVATE_H
 
-#define _MODBUS_TCP_HEADER_LENGTH      7
+#define _MODBUS_TCP_HEADER_LENGTH 7
 #define _MODBUS_TCP_PRESET_REQ_LENGTH 12
-#define _MODBUS_TCP_PRESET_RSP_LENGTH  8
+#define _MODBUS_TCP_PRESET_RSP_LENGTH 8
 
-#define _MODBUS_TCP_CHECKSUM_LENGTH    0
+#define _MODBUS_TCP_CHECKSUM_LENGTH 0
 
 /* In both structures, the transaction ID must be placed on first position
    to have a quick access not dependant of the TCP backend */
-typedef struct _modbus_tcp {
+typedef struct _modbus_tcp
+{
     /* Extract from MODBUS Messaging on TCP/IP Implementation Guide V1.0b
        (page 23/46):
        The transaction identifier is used to associate the future response
@@ -27,10 +28,11 @@ typedef struct _modbus_tcp {
     char ip[16];
 } modbus_tcp_t;
 
-#define _MODBUS_TCP_PI_NODE_LENGTH    1025
-#define _MODBUS_TCP_PI_SERVICE_LENGTH   32
+#define _MODBUS_TCP_PI_NODE_LENGTH 1025
+#define _MODBUS_TCP_PI_SERVICE_LENGTH 32
 
-typedef struct _modbus_tcp_pi {
+typedef struct _modbus_tcp_pi
+{
     /* Transaction ID */
     uint16_t t_id;
     /* TCP port */
