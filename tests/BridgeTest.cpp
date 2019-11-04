87a50d11d840d2180a5f5a487e53efa4d16202d0
nexylmao <nenadvvvvvvv@gmail.com>
2019-11-04 17:17:22 +0100

    Scaffold Bridge tests

--- tests/BridgeTest.cpp
+++ tests/BridgeTest.cpp
@@ -4,25 +4,35 @@
 
 #define private public
 #define protected public
-#include "MockModbusClient.h"
 #include "modbus/ModbusBridge.h"
-
 #undef private
 #undef protected
 
+#include "MockModbusClient.h"
 #include <gtest/gtest.h>
 
 class BridgeTest : public ::testing::Test
 {
 public:
-    void SetUp() override {}
+    void SetUp() override
+    {
+        const uint16_t clientTimeout = 250;
+        const uint16_t readPeriod = 1000;
+        std::vector<wolkabout::ModbusRegisterMapping> mappings;
 
-private:
-    MockModbusClient modbusClient;
-    wolkabout::ModbusBridge modbusBridge;
+        modbusClient =
+          std::unique_ptr<MockModbusClient>(new MockModbusClient(std::chrono::milliseconds(clientTimeout)));
+        modbusBridge = std::make_shared<wolkabout::ModbusBridge>(*modbusClient, std::move(mappings),
+                                                                 std::chrono::milliseconds(readPeriod));
+    }
+
+    std::unique_ptr<MockModbusClient> modbusClient;
+    std::shared_ptr<wolkabout::ModbusBridge> modbusBridge;
 };
 
-TEST(BridgeTest, SimpleTest)
+TEST_F(BridgeTest, SimpleTest)
 {
-    std::cerr << "Hello from test!" << std::endl;
+    //    modbusBridge->onSensorReading([&](const std::string& reference, const std::string& value) {
+    //
+    //    });
 }
