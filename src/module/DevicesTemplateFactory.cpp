2ec72e9307b94546453f646f08d10da78a8fbac3
nvuletic <nenad.vuletic@wolkabout.com>
2020-02-19 11:07:13 +0100

    Tidy up Template Factory

--- src/module/DevicesTemplateFactory.cpp
+++ src/module/DevicesTemplateFactory.cpp
@@ -19,6 +19,117 @@ DataType DevicesTemplateFactory::getDataTypeFromRegisterType(ModbusRegisterMappi
     }
 }
 
+bool DevicesTemplateFactory::processDefaultMapping(const ModbusRegisterMapping& mapping, const DataType& dataType,
+                                                   std::vector<SensorTemplate>& sensorTemplates,
+                                                   std::vector<ActuatorTemplate>& actuatorTemplates)
+{
+    switch (mapping.getRegisterType())
+    {
+    case ModbusRegisterMapping::RegisterType::HOLDING_REGISTER_ACTUATOR:
+    case ModbusRegisterMapping::RegisterType::COIL:
+    {
+        actuatorTemplates.emplace_back(mapping.getName(), mapping.getReference(), dataType, mapping.getDescription(),
+                                       mapping.getMinimum(), mapping.getMaximum());
+        return true;
+    }
+    case ModbusRegisterMapping::RegisterType::HOLDING_REGISTER_SENSOR:
+    case ModbusRegisterMapping::RegisterType::INPUT_REGISTER:
+    case ModbusRegisterMapping::RegisterType::INPUT_CONTACT:
+    {
+        sensorTemplates.emplace_back(mapping.getName(), mapping.getReference(), dataType, mapping.getDescription(),
+                                     mapping.getMinimum(), mapping.getMaximum());
+        return true;
+    }
+    default:
+    {
+        throw std::logic_error("WolkGatewayModbusModule Application: Mapping with reference '" +
+                               mapping.getReference() + "' not added to device manifest - Unknown register type");
+    }
+    }
+}
+
+bool DevicesTemplateFactory::processSensorMapping(const ModbusRegisterMapping& mapping, const DataType& dataType,
+                                                  std::vector<SensorTemplate>& sensorTemplates)
+{
+    switch (mapping.getRegisterType())
+    {
+    case ModbusRegisterMapping::RegisterType::INPUT_CONTACT:
+    case ModbusRegisterMapping::RegisterType::INPUT_REGISTER:
+    case ModbusRegisterMapping::RegisterType::HOLDING_REGISTER_SENSOR:
+        sensorTemplates.emplace_back(mapping.getName(), mapping.getReference(), dataType, mapping.getDescription(),
+                                     mapping.getMinimum(), mapping.getMaximum());
+        return true;
+    default:
+        throw std::logic_error("WolkGatewayModbusModule Application: Mapping with reference '" +
+                               mapping.getReference() +
+                               "' not added to device manifest - Incompatible Mapping and Register type combination");
+    }
+}
+
+bool DevicesTemplateFactory::processActuatorMapping(const ModbusRegisterMapping& mapping, const DataType& dataType,
+                                                    std::vector<ActuatorTemplate>& actuatorTemplates)
+{
+    switch (mapping.getRegisterType())
+    {
+    case ModbusRegisterMapping::RegisterType::COIL:
+    case ModbusRegisterMapping::RegisterType::HOLDING_REGISTER_ACTUATOR:
+        actuatorTemplates.emplace_back(mapping.getName(), mapping.getReference(), dataType, mapping.getDescription(),
+                                       mapping.getMinimum(), mapping.getMaximum());
+        return true;
+    default:
+        throw std::logic_error("WolkGatewayModbusModule Application: Mapping with reference '" +
+                               mapping.getReference() +
+                               "' not added to device manifest - Incompatible Mapping and Register type combination");
+    }
+}
+
+bool DevicesTemplateFactory::processAlarmMapping(const ModbusRegisterMapping& mapping, const DataType& dataType,
+                                                 std::vector<AlarmTemplate>& alarmTemplates)
+{
+    switch (mapping.getRegisterType())
+    {
+    case ModbusRegisterMapping::RegisterType::INPUT_CONTACT:
+        alarmTemplates.emplace_back(mapping.getName(), mapping.getReference(), mapping.getDescription());
+        return true;
+    default:
+        throw std::logic_error("WolkGatewayModbusModule Application: Mapping with reference '" +
+                               mapping.getReference() +
+                               "' not added to device manifest - Incompatible Mapping and Register type combination");
+    }
+}
+
+bool DevicesTemplateFactory::processConfigurationMapping(const ModbusRegisterMapping& mapping, const DataType& dataType,
+                                                         std::vector<ConfigurationTemplate>& configurationTemplates)
+{
+    switch (mapping.getRegisterType())
+    {
+    case ModbusRegisterMapping::RegisterType::COIL:
+    case ModbusRegisterMapping::RegisterType::HOLDING_REGISTER_ACTUATOR:
+        if (mapping.getLabelsAndAddresses().empty())
+        {
+            configurationTemplates.emplace_back(mapping.getName(), mapping.getReference(), dataType,
+                                                mapping.getDescription(), std::string(""), mapping.getMinimum(),
+                                                mapping.getMaximum());
+        }
+        else
+        {
+            std::vector<std::string> labels;
+            for (auto const& kvp : mapping.getLabelsAndAddresses())
+            {
+                labels.push_back(kvp.first);
+            }
+            configurationTemplates.emplace_back(mapping.getName(), mapping.getReference(), dataType,
+                                                mapping.getDescription(), std::string(""), labels, mapping.getMinimum(),
+                                                mapping.getMaximum());
+        }
+        return true;
+    default:
+        throw std::logic_error("WolkGatewayModbusModule Application: Mapping with reference '" +
+                               mapping.getReference() +
+                               "' not added to device manifest - Incompatible Mapping and Register type combination");
+    }
+}
+
 std::unique_ptr<DeviceTemplate> DevicesTemplateFactory::makeTemplateFromDeviceConfigTemplate(
   DevicesConfigurationTemplate& configTemplate)
 {
@@ -27,7 +138,6 @@ std::unique_ptr<DeviceTemplate> DevicesTemplateFactory::makeTemplateFromDeviceCo
     std::vector<AlarmTemplate> alarmTemplates;
     std::vector<ConfigurationTemplate> configurationTemplates;
 
-    // TODO TIDY THIS UP!
     for (const ModbusRegisterMapping& mapping : configTemplate.getMappings())
     {
         auto mappingType = mapping.getMappingType();
@@ -38,125 +148,34 @@ std::unique_ptr<DeviceTemplate> DevicesTemplateFactory::makeTemplateFromDeviceCo
         switch (mappingType)
         {
         case ModbusRegisterMapping::MappingType::DEFAULT:
-            switch (registerType)
+            if (processDefaultMapping(mapping, dataType, sensorTemplates, actuatorTemplates))
             {
-            case ModbusRegisterMapping::RegisterType::HOLDING_REGISTER_ACTUATOR:
-            {
-                actuatorTemplates.emplace_back(mapping.getName(), mapping.getReference(), dataType,
-                                               mapping.getDescription(), mapping.getMinimum(), mapping.getMaximum());
                 break;
             }
-
-            case ModbusRegisterMapping::RegisterType::COIL:
-            {
-                actuatorTemplates.emplace_back(mapping.getName(), mapping.getReference(), dataType,
-                                               mapping.getDescription(), mapping.getMinimum(), mapping.getMaximum());
-                break;
-            }
-
-            case ModbusRegisterMapping::RegisterType::HOLDING_REGISTER_SENSOR:
-            {
-                sensorTemplates.emplace_back(mapping.getName(), mapping.getReference(), dataType,
-                                             mapping.getDescription(), mapping.getMinimum(), mapping.getMaximum());
-                break;
-            }
-
-            case ModbusRegisterMapping::RegisterType::INPUT_REGISTER:
-            {
-                sensorTemplates.emplace_back(mapping.getName(), mapping.getReference(), dataType,
-                                             mapping.getDescription(), mapping.getMinimum(), mapping.getMaximum());
-                break;
-            }
-
-            case ModbusRegisterMapping::RegisterType::INPUT_CONTACT:
-            {
-                sensorTemplates.emplace_back(mapping.getName(), mapping.getReference(), dataType,
-                                             mapping.getDescription(), mapping.getMinimum(), mapping.getMaximum());
-                break;
-            }
-
-            default:
-            {
-                throw std::logic_error("WolkGatewayModbusModule Application: Mapping with reference '" +
-                                       mapping.getReference() +
-                                       "' not added to device manifest - Unknown register type");
-            }
-            }
-            break;
 
         case ModbusRegisterMapping::MappingType::SENSOR:
-            switch (registerType)
+            if (processSensorMapping(mapping, dataType, sensorTemplates))
             {
-            case ModbusRegisterMapping::RegisterType::INPUT_CONTACT:
-            case ModbusRegisterMapping::RegisterType::INPUT_REGISTER:
-            case ModbusRegisterMapping::RegisterType::HOLDING_REGISTER_SENSOR:
-                sensorTemplates.emplace_back(mapping.getName(), mapping.getReference(), dataType,
-                                             mapping.getDescription(), mapping.getMinimum(), mapping.getMaximum());
                 break;
-            default:
-                throw std::logic_error(
-                  "WolkGatewayModbusModule Application: Mapping with reference '" + mapping.getReference() +
-                  "' not added to device manifest - Incompatible Mapping and Register type combination");
             }
-            break;
 
         case ModbusRegisterMapping::MappingType::ACTUATOR:
-            switch (registerType)
+            if (processActuatorMapping(mapping, dataType, actuatorTemplates))
             {
-            case ModbusRegisterMapping::RegisterType::COIL:
-            case ModbusRegisterMapping::RegisterType::HOLDING_REGISTER_ACTUATOR:
-                actuatorTemplates.emplace_back(mapping.getName(), mapping.getReference(), dataType,
-                                               mapping.getDescription(), mapping.getMinimum(), mapping.getMaximum());
                 break;
-            default:
-                throw std::logic_error(
-                  "WolkGatewayModbusModule Application: Mapping with reference '" + mapping.getReference() +
-                  "' not added to device manifest - Incompatible Mapping and Register type combination");
             }
-            break;
 
         case ModbusRegisterMapping::MappingType::ALARM:
-            switch (registerType)
+            if (processAlarmMapping(mapping, dataType, alarmTemplates))
             {
-            case ModbusRegisterMapping::RegisterType::INPUT_CONTACT:
-                alarmTemplates.emplace_back(mapping.getName(), mapping.getReference(), mapping.getDescription());
                 break;
-            default:
-                throw std::logic_error(
-                  "WolkGatewayModbusModule Application: Mapping with reference '" + mapping.getReference() +
-                  "' not added to device manifest - Incompatible Mapping and Register type combination");
             }
-            break;
 
         case ModbusRegisterMapping::MappingType::CONFIGURATION:
-            switch (registerType)
+            if (processConfigurationMapping(mapping, dataType, configurationTemplates))
             {
-            case ModbusRegisterMapping::RegisterType::COIL:
-            case ModbusRegisterMapping::RegisterType::HOLDING_REGISTER_ACTUATOR:
-                if (mapping.getLabelsAndAddresses().empty())
-                {
-                    configurationTemplates.emplace_back(mapping.getName(), mapping.getReference(), dataType,
-                                                        mapping.getDescription(), std::string(""), mapping.getMinimum(),
-                                                        mapping.getMaximum());
-                }
-                else
-                {
-                    std::vector<std::string> labels;
-                    for (auto const& kvp : mapping.getLabelsAndAddresses())
-                    {
-                        labels.push_back(kvp.first);
-                    }
-                    configurationTemplates.emplace_back(mapping.getName(), mapping.getReference(), dataType,
-                                                        mapping.getDescription(), std::string(""), labels,
-                                                        mapping.getMinimum(), mapping.getMaximum());
-                }
                 break;
-            default:
-                throw std::logic_error(
-                  "WolkGatewayModbusModule Application: Mapping with reference '" + mapping.getReference() +
-                  "' not added to device manifest - Incompatible Mapping and Register type combination");
             }
-            break;
 
         default:
             break;
--- src/module/DevicesTemplateFactory.h
+++ src/module/DevicesTemplateFactory.h
@@ -14,6 +14,22 @@ class DevicesTemplateFactory
 {
     static DataType getDataTypeFromRegisterType(ModbusRegisterMapping::RegisterType registerType);
 
+    static bool processDefaultMapping(const ModbusRegisterMapping& mapping, const DataType& dataType,
+                                      std::vector<SensorTemplate>& sensorTemplates,
+                                      std::vector<ActuatorTemplate>& actuatorTemplates);
+
+    static bool processSensorMapping(const ModbusRegisterMapping& mapping, const DataType& dataType,
+                                     std::vector<SensorTemplate>& sensorTemplates);
+
+    static bool processActuatorMapping(const ModbusRegisterMapping& mapping, const DataType& dataType,
+                                       std::vector<ActuatorTemplate>& actuatorTemplates);
+
+    static bool processAlarmMapping(const ModbusRegisterMapping& mapping, const DataType& dataType,
+                                    std::vector<AlarmTemplate>& alarmTemplates);
+
+    static bool processConfigurationMapping(const ModbusRegisterMapping& mapping, const DataType& dataType,
+                                            std::vector<ConfigurationTemplate>& configurationTemplates);
+
     static std::unique_ptr<wolkabout::DeviceTemplate> makeTemplateFromDeviceConfigTemplate(
       DevicesConfigurationTemplate& configTemplate);
 };
