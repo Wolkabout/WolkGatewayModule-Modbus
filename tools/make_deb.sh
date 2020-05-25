b94d25d57f12f04288dc0afd4acfcdf874ba618f
nvuletic <nenad.vuletic@wolkabout.com>
2020-05-25 15:12:58 +0200

    Updates

--- tools/make_deb.sh
+++ tools/make_deb.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+if [ $( dpkg -l debhelper | wc -l ) -le 6 ]
+then
+  apt install debhelper -y || exit
+fi
+
+if [ $( dpkg -l devscripts | wc -l ) -le 6 ]
+then
+  apt install devscripts -y || exit
+fi
+
 mkdir -p ./tmp-wgmm-deb
-cd ./tmp-wgmm-deb
+cd ./tmp-wgmm-deb || exit
 git clone https://github.com/Wolkabout/WolkGatewayModule-Modbus --recurse-submodules
-cd ./WolkGatewayModule-Modbus
+cd ./WolkGatewayModule-Modbus || exit
 # TODO Remove when merge to master
 git checkout debian_package
 debuild -us -uc -b
