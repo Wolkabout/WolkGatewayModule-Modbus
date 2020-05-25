a5939633973755ee20e1bddd560a89eb192f8d8a
nvuletic <nenad.vuletic@wolkabout.com>
2020-05-25 12:49:44 +0200

    Now it builds

--- tools/make_deb.sh
+++ tools/make_deb.sh
@@ -4,8 +4,11 @@ mkdir -p ./tmp-wgmm-deb
 cd ./tmp-wgmm-deb
 git clone https://github.com/Wolkabout/WolkGatewayModule-Modbus --recurse-submodules
 cd ./WolkGatewayModule-Modbus
+# TODO Remove when merge to master
 git checkout debian_package
 debuild -us -uc -b
-cd ../../
+cd ../
+mv *.deb ..
+cd ../
 rm -rf ./tmp-wgmm-deb
 
