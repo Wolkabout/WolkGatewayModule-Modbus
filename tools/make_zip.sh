1f41462ed2e97bf0b442094d1fb2a21660529e37
nvuletic <nenad.vuletic@wolkabout.com>
2020-05-26 15:36:50 +0200

    Make_zip in local folder

--- tools/make_zip.sh
+++ tools/make_zip.sh
@@ -2,10 +2,10 @@
 
 # This script is used to make a release .zip file for WolkGatewayModule-Modbus release
 
-mkdir -p ~/tmp-wgmm
-cd ~/tmp-wgmm
+mkdir -p ./tmp-wgmm
+cd ./tmp-wgmm
 git clone https://github.com/Wolkabout/WolkGatewayModule-Modbus --recurse-submodules
-cd ~/tmp-wgmm/WolkGatewayModule-Modbus
+cd ./WolkGatewayModule-Modbus
 
 # TODO remove line
 git checkout debian_package
@@ -13,5 +13,6 @@ git checkout debian_package
 filename="WolkGateway-ModbusModule-v$(cat RELEASE_NOTES.txt | grep "**Version" | head -1 | sed -e "s/**Version //" | sed -e "s/\*\*//").zip"
 echo "filename: $filename"
 zip -qr $filename *
-mv "$filename" ~
-rm -rf ~/tmp-wgmm
+mv "$filename" ../..
+cd ../..
+rm -rf ./tmp-wgmm
