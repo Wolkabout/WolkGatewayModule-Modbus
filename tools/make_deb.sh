e7dd40be7b8c2cd7703a01e7f60f4ce0e86a7210
nvuletic <nenad.vuletic@wolkabout.com>
2020-05-27 08:48:28 +0200

    Point to master with scripts

--- tools/make_deb.sh
+++ tools/make_deb.sh
@@ -6,13 +6,8 @@ mkdir -p ./tmp-wgmm-deb
 cd ./tmp-wgmm-deb || exit
 git clone https://github.com/Wolkabout/WolkGatewayModule-Modbus --recurse-submodules
 cd ./WolkGatewayModule-Modbus || exit
-
-# TODO Remove when merge to master
-git checkout debian_package
-
 debuild -us -uc -b -j$(nproc)
 cd ../
 mv *.deb ..
 cd ../
 rm -rf ./tmp-wgmm-deb
-
--- tools/make_zip.sh
+++ tools/make_zip.sh
@@ -3,13 +3,9 @@
 # This script is used to make a release .zip file for WolkGatewayModule-Modbus release
 
 mkdir -p ./tmp-wgmm
-cd ./tmp-wgmm
+cd ./tmp-wgmm || exit
 git clone https://github.com/Wolkabout/WolkGatewayModule-Modbus --recurse-submodules
-cd ./WolkGatewayModule-Modbus
-
-# TODO remove line
-git checkout debian_package
-
+cd ./WolkGatewayModule-Modbus || exit
 filename="WolkGateway-ModbusModule-v$(cat RELEASE_NOTES.txt | grep "**Version" | head -1 | sed -e "s/**Version //" | sed -e "s/\*\*//").zip"
 echo "filename: $filename"
 zip -qr $filename *
