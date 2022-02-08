f6761b628b4422fd7ec413e5b1af0cc0ef2488ef
nanavuletic <nenad.vuletic@wolkabout.com>
2022-02-08 17:02:41 +0100

    Redid the project a little bit, required changes on `MoreModbus`

--- docs/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['deserializemappingtype_39',['deserializeMappingType',['../classwolkabout_1_1MappingTypeConversion.html#abf03bb56544ce829e6fe5a9468b21769',1,'wolkabout::MappingTypeConversion']]]
-];
